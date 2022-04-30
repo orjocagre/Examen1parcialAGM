a98fa260786071caa3a9585979829b501ce4e1a3
orjocagre <orlandocaceres114@gmail.com>
2022-04-29 23:56:07 -0600

    lo que se pudo

---
+++ aplicacion.js
@@ -0,0 +1,9 @@
+function calcular(){
+
+    let sbm = document.getElementById("salario").value;
+    let di = sbm * 0.07;
+
+    document.getElementById("sbm").value=sbm;
+    document.getElementById("di").value=di;
+   
+}
